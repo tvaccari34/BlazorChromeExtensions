66b8b0e62875c68b6ce8279daa3cf2082ca44145
Tiago Vaccari <tiago.vaccari@outlook.com>
2024-07-03 20:25:52 -0400

    Initial Commit

---
+++ MyBlazorExtension/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyBlazorExtension
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
+            builder.UseBrowserExtension(browserExtension =>
+            {
+                builder.RootComponents.Add<App>("#app");
+                builder.RootComponents.Add<HeadOutlet>("head::after");
+            });
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            await builder.Build().RunAsync();
+        }
+    }
+}
